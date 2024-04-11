38bc7e426521c365401f642fbcde2966317291b8
Vyacheslav <nodegopher@gmail.com>
2024-04-11 20:46:03 +0300

    f

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/morheus9/go_grpc
+
+go 1.22.2
---
+++ src/main/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/morheus9/go_grpc/src/main/find_data"
+)
+
+func main() {
+
+	strconv.Itoa(find_data.Find_data())
+	fmt.Printf("F")
+}
